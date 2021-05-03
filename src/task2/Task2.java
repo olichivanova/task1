0ad2ce06a3828030dbe23fa83fa8fada9f541291
= <olich_ivanova@mail.ru>
2021-05-03 19:44:39 +0300

    five

--- src/task2/Task2.java
+++ src/task2/Task2.java
@@ -3,37 +3,71 @@ package task2;
 public class Task2 {
     public static void main(String[] args) {
         int[][] field ={
-                {2, 3, 2, 2},
-                {2, 1, 3, 3},
-                {3, 2, 3, 2},
-                {2, 3, 2, 2},
+                {4, 4, 4, 4},
+                {4, 2, 2, 4},
+                {4, 2, 2, 4},
+                {2, 1, 1, 2},
+                {2, 4, 2, 2},
+        };
 
-
+        int[][] field0 ={
+                {4, 4, 2, 2},
+                {4, 2, 1, 4},
+                {4, 2, 1, 2},
+                {4, 2, 1, 2},
+                {4, 4, 2, 2},
         };
         int[][] field1 ={
                 {1, 2, 2, 3},
                 {2, 1, 1, 4},
                 {4, 5, 6, 2},
-
         };
         int[][] field2 ={
                 {2, 2, 3, 2},
                 {3, 1, 2, 4},
                 {4, 3, 3, 2},
-
         };
-      print(findWater(field));
+
+        int[][] field3 ={
+                {2, 3, 4, 2},
+                {2, 1, 3, 4},
+                {3, 2, 3, 2},
+                {2, 4, 2, 2},
+        };
+
+        int[][] field4 ={
+                {3, 3, 3, 3},
+                {3, 1, 3, 3},
+                {3, 2, 1, 2},
+                {3, 3, 3, 2},
+        };
+        int[][] field5 ={
+                {3, 3, 3, 3},
+                {3, 3, 1, 3},
+                {3, 1, 2, 3},
+                {3, 3, 2, 2},
+        };
+
+    print(findWater(field));
+        System.out.println();
+        print(findWater(field0));
         System.out.println();
        print(findWater(field1));
         System.out.println();
         print(findWater(field2));
+        System.out.println();
+        print(findWater(field3));
+        System.out.println();
+        print(findWater(field4));
+        System.out.println();
+        print(findWater(field5));
 
 
     }
     public  static int[][] findWater (int[][] field){
         if (field== null)
             return null;
-        int [][] water = new int [field.length] [field[0].length]   ;
+        int [][] water = new int [field.length] [field[0].length] ;
 
                for (int k = 0; k<field.length; k++){
                     for (int z = 0; z< field[0].length; z++){
@@ -48,7 +82,7 @@ public class Task2 {
                              if (field[k][m] == 0)
                             {water[k][z] = 0;
                             break;}
-                            if (field[k][m]> field[k][m+1]){
+                            if (field[k][m]>= field[k][m+1]){
                                 if (field[k][m]>left)
                                     left= field[k][m];
                             }
@@ -62,7 +96,7 @@ public class Task2 {
                             if (field[k][n] == 0)
                             {  water[k][z] = 0;
                             break;}
-                            if (field[k][n]> field[k][n-1]  ){
+                            if (field[k][n]>= field[k][n-1]  ){
                                 if (field[k][n]>right)
                                     right = field[k][n];
                             }
@@ -76,7 +110,7 @@ public class Task2 {
                              if (field[q][z]==0)
                             { water[k][z] = 0;
                             break;}
-                            if (field[q][z]>field[q+1][z]){
+                            if (field[q][z]>=field[q+1][z]){
                                 if (field[q][z]>up)
                                     up = field[q][z];
                             }
@@ -91,7 +125,7 @@ public class Task2 {
                             if (field[r][z]==0)
                             { water[k][z] = 0;
                             break;}
-                            if (field[r][z]>field[r-1][z]){
+                            if (field[r][z]>=field[r-1][z]){
                                 if (field[r][z]>down)
                                     down = field[r][z]; }
                             else{
@@ -102,12 +136,28 @@ public class Task2 {
 
                         }
                         int min = Math.min(Math.min(up,down), Math.min(right,left));
-                          if(field[k][z]>=water[k][z-1] && z-1!=0 && min>field[k][z-1] )
+                        if (min == field[k][z])
+                        {water[k][z] = 0;
+                            if (water[k][z-1]>=water[k][z])
+                            { water[k][z-1]=min;
+                            if (water[k][z-1] == field[k][z-1])
+                                water[k][z-1]=0; }
+                            if (water[k-1][z]>=water[k][z])
+                            { water[k-1][z]=min;
+                             if (water[k-1][z]==field[k-1][z])
+                            water[k-1][z]=0;}
+                            }
+                         else if(field[k][z]>=water[k][z-1] && z-1!=0 && min>field[k][z-1] )
                             water[k][z] = 0;
-                          else if( field[k][z]>=water[k-1][z] && k-1!=0 && min>field[k-1][z])
+                         else if( field[k][z]>=water[k-1][z] && k-1!=0 && min>field[k-1][z])
                               water[k][z] = 0;
-                        else
+                         else
                             water[k][z] = min;
+
+                     if ( water[k][z] < water[k-1][z] && field[k][z] < field[k-1][z])
+                        water[k-1][z] = 0;
+                     if ( water[k][z] <water[k][z-1] && field[k][z] <field[k][z-1] )
+                          water[k][z-1] = 0;
                     }}
                 }
 
